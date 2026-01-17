f91b917da9d076bc885966104fd1f12df78bc421
Marc Prud'hommeaux <mwp1@cornell.edu>
2026-01-17 13:18:37 -0500

    Update SHA256 hash implementation

--- Tests/SkipBuildTests/SkipBuildTests.swift
+++ Tests/SkipBuildTests/SkipBuildTests.swift
@@ -15,10 +15,54 @@ final class SkipBuildTests: XCTestCase {
     }
 
     func testSHA256() throws {
-        let tmpFile = URL(fileURLWithPath: NSTemporaryDirectory().appending("/" + UUID().uuidString))
-        try "Hello World".write(to: tmpFile, atomically: true, encoding: .utf8)
-        defer { try? FileManager.default.removeItem(at: tmpFile) }
-        XCTAssertEqual("a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e", try tmpFile.SHA256Hash())
+        do {
+            let tmpFile = URL(fileURLWithPath: NSTemporaryDirectory().appending("/" + UUID().uuidString))
+            try "Hello World".write(to: tmpFile, atomically: true, encoding: .utf8)
+            defer { try? FileManager.default.removeItem(at: tmpFile) }
+            XCTAssertEqual("a591a6d40bf420404a011733cfb7b190d62c65bf0bcda32b57b277d9ad9f146e", try tmpFile.SHA256Hash())
+        }
+
+        do {
+            let msg = "".data(using: .utf8)!
+            let result = msg.SHA256Hash()
+            let expected = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855" // echo -n "" | openssl dgst -sha256
+            XCTAssertEqual(result, expected, "Invalid conversion from msg to sha256")
+        }
+
+        do {
+            let msg = "foobar".data(using: .utf8)!
+            let result = msg.SHA256Hash()
+            let expected = "c3ab8ff13720e8ad9047dd39466b3c8974e592c2fa383d4a3960714caef0c4f2" // echo -n "foobar" | openssl dgst -sha256
+            XCTAssertEqual(result, expected, "Invalid conversion from msg to sha256")
+        }
+
+        do {
+            let msg = "æøå".data(using: .utf8)!
+            let result = msg.SHA256Hash()
+            let expected = "6c228cdba89548a1af198f33819536422fb01b66e51f761cf2ec38d1fb4178a6" // echo -n "æøå" | openssl dgst -sha256
+            XCTAssertEqual(result, expected, "Invalid conversion from msg to sha256")
+        }
+
+        do {
+            let msg = "KfZ=Day*q4MsZ=_xRy4G_Uefk?^Ytr&2xL*RYY%VLyB_&c7R_dr&J+8A79suf=^".data(using: .utf8)!
+            let result = msg.SHA256Hash()
+            let expected = "b754632a872b3f5ddb0e1e24b531e35eb334ee3c2957618ac4a2ac4047ed6127" // echo -n "KfZ=Day*q4MsZ=_xRy4G_Uefk?^Ytr&2xL*RYY%VLyB_&c7R_dr&J+8A79suf=^" | openssl dgst -sha256
+            XCTAssertEqual(result, expected, "Invalid conversion from msg to sha256")
+        }
+
+        do {
+            let msg = "Lorem ipsum dolor sit amet, suas consequuntur mei ad, duo eu noluisse adolescens temporibus. Mutat fuisset constituam te vis. Animal meliore cu has, ius ad recusabo complectitur. Eam at persius inermis sensibus. Mea at velit nobis dolor, vitae omnium eos an, ei dolorum pertinacia nec.".data(using: .utf8)!
+            let result = msg.SHA256Hash()
+            let expected = "31902eb17aa07165b645553c14b985c1908c7d8f4f5178de61a3232f09940df7" // echo -n "Lorem ipsum dolor sit amet, suas consequuntur mei ad, duo eu noluisse adolescens temporibus. Mutat fuisset constituam te vis. Animal meliore cu has, ius ad recusabo complectitur. Eam at persius inermis sensibus. Mea at velit nobis dolor, vitae omnium eos an, ei dolorum pertinacia nec." | openssl dgst -sha256
+            XCTAssertEqual(result, expected, "Invalid conversion from msg to sha256")
+        }
+
+        do {
+            let msg = "0".data(using: .utf8)!
+            let result = msg.SHA256Hash()
+            let expected = "5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9" // echo -n "0" | openssl dgst -sha256
+            XCTAssertEqual(result, expected, "Invalid conversion from msg to sha256")
+        }
     }
 
     func testPadString() {
